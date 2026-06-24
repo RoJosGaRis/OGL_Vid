3d91f495f5bff814f570954ebf1323cf1ff2a8c6
Roberto García <A01284731@tec.mx>
2026-06-23 18:39:58 -0600

    Add new systems and update CMake configuration
    - Introduced MotionSystem and CameraSystem classes for handling motion and camera functionalities.
    - Added app controller to manage application logic.
    - Updated CMakeLists.txt to include new source files.
    - Modified .gitignore to exclude cmake/ directory.

---
+++ src/controller/app.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "../config.h"
+
+#include "../components/camera_component.h"
+#include "../components/physics_component.h"
+#include "../components/render_component.h"
+#include "../components/transform_component.h"
